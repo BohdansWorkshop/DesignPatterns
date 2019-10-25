fae26103c1a45c55952847d1c7632b6bbfe7497e
Bohdan <universalshon@gmail.com>
2019-10-25 23:46:07 +0300

    -Added structural patterns folder
    -Added adapter pattern template

---
+++ DesignPatterns/Structural/1.Adapter/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace _1.Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var drink = new Drink();
+            drink.UseDrink();
+
+            var juice = new Juice();
+            //juice.DrinkJuice();
+
+            var adapted = new JuiceToDrink(juice);
+            adapted.UseDrink();
+        }
+    }
+
+    #region UML Implementation
+
+    class Client
+    {
+        void Main()
+        {
+            Target target = new Adapter();
+            target.TargetRequest();
+        }
+    }
+    //class which needs to be adapted;
+    class Adaptee
+    {
+        public void AdapteeRequest()
+        {
+
+        }
+    }
+
+    //the destination of adaptation process
+    class Target
+    {
+        public virtual void TargetRequest()
+        {
+
+        }
+    }
+
+    class Adapter : Target
+    {
+        Adaptee _adaptee = new Adaptee();
+
+        public override void TargetRequest()
+        {
+            _adaptee.AdapteeRequest();
+        }
+
+    }
+
+    #endregion
+
+    #region Custom Implementation
+
+    public interface IDrink
+    {
+        void UseDrink();
+    }
+
+    public class Drink : IDrink
+    {
+        public void UseDrink()
+        {
+            Console.WriteLine("Drink default drink");
+        }
+    }
+
+    public interface IJuice
+    {
+        void DrinkJuice();
+    }
+
+    public class Juice : IJuice
+    {
+        public void DrinkJuice()
+        {
+            Console.WriteLine("Drink Juice");
+        }
+    }
+
+
+    public class JuiceToDrink : IDrink
+    {
+        IJuice _juice;
+        public JuiceToDrink(Juice juice)
+        {
+            _juice = juice;
+        }
+
+        public void UseDrink()
+        {
+            Console.WriteLine("Adapted juice to drink");
+            _juice.DrinkJuice();
+        }
+    }
+
+
+    #endregion
+}
