8f00c937c964bb883d5cee52d363c61db069f0e9
Bohdan <universalshon@gmail.com>
2019-10-27 20:32:04 +0200

    Proxy template

---
+++ DesignPatterns/Structural/5.Proxy/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _5.Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subject = new Proxy(null);
+            subject.Request();
+        }
+    }
+
+    #region UML Implementation
+
+    abstract class Subject
+    {
+        public abstract void Request();
+    }
+
+    class ConcreteSubject : Subject
+    {
+        public override void Request()
+        {
+            Console.WriteLine("Subject received request");
+        }
+    }
+
+    class Proxy : Subject
+    {
+        ConcreteSubject _subject;
+        public Proxy(ConcreteSubject subject)
+        {
+            _subject = subject;
+        }
+        public override void Request()
+        {
+            if(_subject == null)
+            {
+                _subject = new ConcreteSubject();
+            }
+            _subject.Request();
+        }
+    }
+
+    #endregion
+
+}
